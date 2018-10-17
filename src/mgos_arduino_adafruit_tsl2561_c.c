3bda99aecf80698d9442a66efca03f5995940bc4
Ted Huntington <thunting@uci.edu>
2018-10-17 14:42:47 -0700

    added different file
    added different file

--- src/mgos_arduino_adafruit_tsl2561_c.c
+++ src/mgos_arduino_adafruit_tsl2561_c.c
@@ -1,10 +1,6 @@
 #include <stdbool.h>
 
 
-bool mgos_tsl2561_init(void) {
-    return true;
-}
-
 bool mgos_arduino_adafruit_tsl2561_init(void) {
     return true;
 }
---
+++ src/mgos_tsl2561_c.c
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+
+
+bool mgos_tsl2561_init(void) {
+    return true;
+}
