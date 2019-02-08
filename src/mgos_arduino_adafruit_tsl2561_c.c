ed82fa48ddeaa388d5ae048dc38896de6aa26a2c
PlantScience <47363300+PlantScience@users.noreply.github.com>
2019-02-07 21:45:24 -0800

    Update and rename mgos_arduino_adafruit_tsl2561_c.c to mgos_arduino_adafruit_tsl25721_c.c

--- src/mgos_arduino_adafruit_tsl2561_c.c
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-
-
-bool mgos_arduino_adafruit_tsl2561_init(void) {
-    return true;
-}
---
+++ src/mgos_arduino_adafruit_tsl25721_c.c
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+
+
+bool mgos_arduino_adafruit_tsl25721_init(void) {
+    return true;
+}
