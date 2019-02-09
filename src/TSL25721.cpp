193bfa6be744c5292c54135dbfdab2ecba3d4606
PlantScience <47363300+PlantScience@users.noreply.github.com>
2019-02-08 22:54:45 -0800

    Update TSL25721.cpp

--- src/TSL25721.cpp
+++ src/TSL25721.cpp
@@ -52,7 +52,8 @@ TSL25721::TSL25721(uint8_t addr) {
     _initialized = false;
     //_integration = TSL25721_INTEGRATIONTIME_13MS;
     _integration = TSL25721_INTEGRATIONTIME_27MS;
-    _gain = TSL25721_GAIN_16X;
+    //_gain = TSL25721_GAIN_16X;
+    _gain = TSL25721_GAIN_120X;
 
     // we cant do wire initialization till later, because we havent loaded Wire yet
 }
